d6c28baa0de1f2616db1015788ea43168668839a
praveen00219 <paru2192000@gmail.com>
2024-12-30 01:24:29 +0530

    updated

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -8,7 +8,9 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {};
+const firebaseConfig = {
+  // Add your firebaseConfig
+};
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
