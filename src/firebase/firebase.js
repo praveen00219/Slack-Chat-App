c9c9379cb81023a421d6d7fc694c1e793fc1c06e
praveen00219 <paru2192000@gmail.com>
2025-02-17 16:35:42 +0530

    for confidencial removed firebase config and put in .env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -9,12 +9,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCxqVF0n90ziJGAH62Q_t6xLMZq7KmzCg4",
-  authDomain: "slack-bf894.firebaseapp.com",
-  projectId: "slack-bf894",
-  storageBucket: "slack-bf894.firebasestorage.app",
-  messagingSenderId: "656047374193",
-  appId: "1:656047374193:web:49a33e2d20bf406b1780d0",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
